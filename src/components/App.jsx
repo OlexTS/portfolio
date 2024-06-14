1695a2f076639c90e48396b9991b24a732a417d4
OlexTS <sashokseven@gmail.com>
2024-06-14 23:08:01 +0300

    added routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import { Suspense, lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+const HomePage = lazy(() => import('./pages/HomePage'));
+const ProjectsPage = lazy(() => import('./pages/ProjectsPage'));
+const NotFound = lazy(() => import('./pages/NotFound'));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      Portfolio
-    </div>
+    <Suspense fallback={<div>'Loading...'</div>}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </Suspense>
   );
 };
