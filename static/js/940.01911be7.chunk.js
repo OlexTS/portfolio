a0b2be769a46d0d6ba91f5b4599306889d5b432f
OlexTS <107061589+OlexTS@users.noreply.github.com>
2024-06-14 20:50:16 +0000

    Deploying to gh-pages from @ OlexTS/portfolio@bf778db992e576aaec5701f8c87ef1bd5e5eee9f 🚀

---
+++ static/js/940.01911be7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[940],{940:function(e,t,r){r.r(t);r(791);var n=r(184);t.default=function(){return(0,n.jsx)("div",{children:"NotFound"})}}}]);
+//# sourceMappingURL=940.01911be7.chunk.js.map
