65272827aeacefe2f185db2833dd1dcbcd0a0b79
OlexTS <sashokseven@gmail.com>
2024-06-16 12:10:58 +0300

    fixed location of loader

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,14 +1,14 @@
 import Footer from 'components/Footer/Footer';
 import Header from 'components/Header/Header';
+import Loader from 'components/Loader/Loader';
 import React, { Suspense } from 'react';
-import { RotatingLines } from 'react-loader-spinner';
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
     <>
       <Header />
-      <Suspense fallback={<RotatingLines />}>
+      <Suspense fallback={<Loader />}>
         <Outlet />
       </Suspense>
       <Footer/>
