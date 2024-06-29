cb1de07d471d402c83595ddbd464bb05a197dbb6
OlexTS <sashokseven@gmail.com>
2024-06-29 18:18:18 +0300

    created HomeView component

---
+++ src/components/HomeView/HomeView.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomeView = () => {
+  return (
+    <main>
+      <section>
+        <h1>Hello everyone!!!</h1>
+        <p>My name is Olexandr and I'm a Frontend Developer!</p>
+      </section>
+    </main>
+  );
+};
+
+export default HomeView;
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,8 +1,9 @@
+import HomeView from 'components/HomeView/HomeView'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div><HomeView/></div>
   )
 }
 
